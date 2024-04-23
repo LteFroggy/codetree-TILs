7c2aab4b113faeeff664ff467f5d244cec7be091
hojong <33508056+LteFroggy@users.noreply.github.com>
2024-04-23 17:52:15 +0900

    Title: 두 반의 평균점수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240423/두 반의 평균점수/average-score-of-two-classes.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> score(N + 1);
+
+    for (int i = 1; i <= N; i++) {
+        cin >> score[i];
+    }
+
+    int a, b;
+    cin >> a >> b;
+
+    double answer = double(score[a] + score[b]) / 2;
+    answer *= 10;
+    answer = round(answer);
+    answer /= 10;
+
+    cout << answer << endl;
+    return 0;
+}
