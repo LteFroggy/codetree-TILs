80a0fd676a1b58a43cc08a0429d8b0994d315e4b
hojong <33508056+LteFroggy@users.noreply.github.com>
2024-04-25 14:37:15 +0900

    Title: 거듭제곱을 출력하는 함수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240425/거듭제곱을 출력하는 함수/function-that-outputs-power-of-two.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+long mul_recur(int a, int b) {
+    if (b == 1)
+        return a;
+    else 
+        return a * mul_recur(a, b - 1);
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    cout << mul_recur(a, b) << endl;
+    return 0;
+}
