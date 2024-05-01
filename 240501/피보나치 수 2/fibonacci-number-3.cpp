a04b891012499078e28eee30d806480346a67a6e
hojong <33508056+LteFroggy@users.noreply.github.com>
2024-05-01 16:56:30 +0900

    Title: 피보나치 수 2, Time: 10ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 240501/피보나치 수 2/fibonacci-number-3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+
+/*
+    n번째 피보나치 구하기
+    재귀로 하자. 아니면 dp
+    이번엔 dp 시도해보기
+*/
+
+using namespace std;
+
+int main() {
+    vector<int> fibonacci;
+    fibonacci.push_back(0);
+    fibonacci.push_back(1);
+
+    int N;
+    cin >> N;
+
+    for (int i = 2; i < N + 1; i++) {
+        if (i == N) {
+            cout << fibonacci[i - 1] + fibonacci[i - 2];
+        }
+
+        else {
+            fibonacci.push_back(fibonacci[i-1] + fibonacci[i-2]);
+        }
+    }
+    
+    return 0;
+}
