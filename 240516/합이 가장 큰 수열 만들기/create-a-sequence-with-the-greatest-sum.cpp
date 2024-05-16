febc21c401e7480e6bd54216f780e72773f77a5d
hojong <33508056+LteFroggy@users.noreply.github.com>
2024-05-16 23:36:51 +0900

    Title: 합이 가장 큰 수열 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240516/합이 가장 큰 수열 만들기/create-a-sequence-with-the-greatest-sum.cpp
+++ 240516/합이 가장 큰 수열 만들기/create-a-sequence-with-the-greatest-sum.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 /*
-    길이가 n인 수열, 수열에서 원소를 뽑아 길이가 가장 긴 오름차순 수열?
+    길이가 n인 수열, 수열에서 원소를 맘대로 뽑아 길이가 가장 긴 오름차순 수열?
     다음 값을 보았을 때 이전 값보다 작으면서 값이 가장 큰 값을 기억하면 된다.
+
+    가장 작은 값을 뽑았을 때 항상 가장 긴 수열이 나올까? 그건 아님.
+    시작 위치를 어디로 잡느냐에 따라 다를 수 있다.
+
+    근데 다 해봤자 1 ~ 1000 더하기이므로 그냥 하자.
 */
 
 int main() {
     int N;
     cin >> N;
+    vector<int> numbers(N);
 
     int max_sum = 0;
     int max_length = 0;
@@ -17,30 +24,29 @@ int main() {
     int length = 0;
     int val_before = 0;
     for (int i = 0; i < N; i++) {
-        int val_now;
-        cin >> val_now;
-        // 이번 값이 이전 값보다 크거나 같다면 오름차순 맞으므로 length 추가하고, sum 더해주기
-        if (val_now >= val_before) {
-            length++;
-            sum += val_now;
-            val_before = val_now;
+        cin >> numbers[i];
+    }
+
+    // 시작 위치를 1번부터 1000번으로 하면서 가능한 최대한 긴 오름차순 수열 만들기를 해볼 것
+    for (int i = 0; i < N; i++) {
+        val_before = numbers[i];
+        sum = numbers[i];
+        length = 1;
+        for (int j = i + 1; j < N; j++) {
+            // 이번 숫자가 이전 숫자보다 크거나 같다면, 그대로 적용
+            if (numbers[j] >= val_before) {
+                val_before = numbers[j];
+                sum += numbers[j];
+                length++;
+            }
         }
 
-        // 이번 값이 이전 값보다 작거나, 마지막 원소라면 값 정리해주기
-        if (val_now < val_before || i == N-1) {
-            // 더 긴 수열을 찾았거나, 길이는 같으나 합이 더 큰 경우 max 갱신
-            if (max_length < length || (max_length == length && max_sum < sum)) {
-                max_length = length;
-                max_sum = sum;
-            }
-
-            // 변수 초기화
-            val_before = val_now;
-            length = 1;
-            sum = val_now;
+        if (max_length < length || (max_length == length && max_sum < sum)) {
+            max_length = length;
+            max_sum = sum;
         }
     }
 
-    cout << max_sum << endl;
+     cout << max_sum << endl;
     return 0;
 }
