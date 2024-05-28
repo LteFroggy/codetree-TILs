b8de9ff958f9c93dfd39eef8c62a2c83f43b4fb9
hojong <33508056+LteFroggy@users.noreply.github.com>
2024-05-28 20:22:18 +0900

    Title: 별표 출력하기 16, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240528/별표 출력하기 16/print-star-16.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+// 힌트 보고 그대로 진행
+int main() {
+    int N;
+    cin >> N;
+
+    // 출력용 for문
+    for (int i = 0; i < N; i++) {
+        // 공백 출력
+        for (int k = 0; k < pow(N, 2) - pow(N - i, 2); k++) {
+            cout << " ";
+        }
+
+        // 별표 출력
+        for (int k = 0; k < pow(N - i, 2); k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
