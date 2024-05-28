8c8aa23241fcf646ccc837c21ab1e93f2c9edb43
hojong <33508056+LteFroggy@users.noreply.github.com>
2024-05-28 14:41:56 +0900

    Title: 경우의수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240528/경우의수 구하기/find-the-number-of-cases.cpp
+++ 240528/경우의수 구하기/find-the-number-of-cases.cpp
@@ -11,23 +11,36 @@ using namespace std;
 
     그냥 M개의 다리 중 N개를 고르는 것이었다. 어차피 N개를 고르면 그에 맞게 건설하는 방법은 하나밖에 나오지 않음.
     mCn이다.
+
+    이걸 그냥 계산하려고 하니 값이 터진다. DP를 사용하자
+    nC0 = 1, nCn = 1이고,
+    (n+1)C(r+1) = nCr + nC(r+1)이다.
+
+    따라서 1C0 = 1, 1C1 = 1이고,
+    2C1은 위의 둘을 더해서 2, 2C0 = 1, 2C2 = 1이다.
+    3C0, 3C3은 모두 1, 3C1은 3, 3C2는 3, 이런 식으로 해나갈 수 있음.
+    
 */
 int main() {
-    long long N, M;
+    int N, M;
     cin >> N >> M;
-    long long answer = 1;
 
-    for (int i = M; i > N; i--) {
-        // cout << answer << " * " << i << " = ";
-        answer *= i;
-        // cout << answer << endl;
-    }
-    for (int i = 2; i <= M - N; i++) {
-        // cout << answer << " / " << i << " = ";
-        answer /= i;
-        // cout << answer << endl;
+    // DP[M][N]은 mCn을 의미한다.
+    vector<vector<int>> DP(M + 1, vector<int>(M + 1, 1));
+
+    for (int i = 2; i <= M; i++) {
+        for (int j = 1; j < i; j++) {
+            DP[i][j] = DP[i - 1][j] + DP[i - 1][j - 1];
+        }
     }
 
-    cout << answer << endl;
+    // for (int i = 1; i <= M; i++) {
+    //     for (int j = 0; j <= i; j++) {
+    //         cout << i << "C" << j << " = " << DP[i][j] << "    ";
+    //     }
+    //     cout << endl;
+    // }
+
+    cout << DP[M][N] << endl;
     return 0;
 }
