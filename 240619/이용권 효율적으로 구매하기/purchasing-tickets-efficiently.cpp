2466f1689eb6b8304a3fe8dff7866411bfccbf3d
hojong <33508056+LteFroggy@users.noreply.github.com>
2024-06-19 23:02:37 +0900

    Title: 이용권 효율적으로 구매하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240619/이용권 효율적으로 구매하기/purchasing-tickets-efficiently.cpp
+++ 240619/이용권 효율적으로 구매하기/purchasing-tickets-efficiently.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <set>
+#include <algorithm>
+#include <climits>
 
 using namespace std;
 
@@ -9,18 +12,16 @@ using namespace std;
     필요한 최소 비용 구하는 프로그램 작성해보자.
 
     역시 DP로 처리해야 할 듯.
-    DP[N] = pair<int, int>로 처리하고, first는 든 돈, second는 쿠폰 개수로 생각하자.
+    DP = vector<vector<int>>로 처리하고, DP[i][j]는 i일에 j개의 쿠폰을 가진 경우의 최소 금액이라 생각하자.
 
-    매일 매일 1일, 3일, 5일권을 살 수 있다. 따라서 1일권을 사서 도착한 경우, 3일권을 사서 도착한 경우, 5일권을 사서 도착한 경우를 모두 고려하자
-    그런데 쉬는 날은???? 고려해야 할까? 아니면 그냥 무시할까? -> 그냥 무시해도 괜찮을지도..
+    i, j별로 1일권, 3일권, 5일권을 구매했을 때 최소금액을 DP[i + a][j + b]에 저장할 것
 
-    DP[i] = min(DP[i - 1].first + 10000, DP[i - 3].first + 25000, DP[i - 5].first + 37000) 으로 볼 수 있다.
-    단, i가 휴일인 경우 DP[i] = DP[i - 1]이고, DP[i - 1].second >= 3인 경우에는 DP[i] = DP[i - 1]을 여기서도 사용 가능하다.
-
-    주어진 경우 베스트는, 1일에 3일권 사기
+    매일 매일 1일, 3일, 5일권을 살 수 있다. 따라서 매일 1일권을 사서 도착한 경우, 3일권을 사서 도착한 경우, 5일권을 사서 도착한 경우를 모두 고려하자
+    
+    주어진 경우 베스트는
     1 ~ 5   5일권 -> 37000, 2
     8 ~ 10  3일권 -> 62000, 3
-    13      5일권 -> 62000, 0
+    13      쿠폰 -> 62000, 0
 */
 
 int main() {
@@ -28,63 +29,62 @@ int main() {
     cin >> N >> M;
 
     vector<int> impos_days(M);
-
     for (int i = 0 ; i < M; i++) cin >> impos_days[i];
 
-    for (auto v : impos_days) cout << v << " ";
-    cout << endl;
+    set<int> impos_set(impos_days.begin(), impos_days.end());
 
-    vector<pair<int, int>> DP(N + 1);
+    // 쿠폰의 최대는 33개이다. 따라서 (N+1) x (33) 사이즈의 벡터를 만듦.
+    vector<vector<int>> DP(N + 1, vector<int>(33, INT_MAX));
 
-    DP[0] = make_pair(0, 0);
-
-    vector<vector<int>> ticket_list {{10000, 1, 0}, {25000, 3, 1}, {37000, 5, 2}};
+    // 0일차 초기화. 0일차에는 쿠폰 0개인 상황만 존재한다.
+    DP[0][0] = 0;
 
     // DP 해나갈 것
     int impos_index = 0;
-    for (int i = 1; i <= N; i++) {
-        // 오늘이 딱히 들어갈 필요 없는 날이라면, 어제 값을 그대로 쓴다.
-        if (impos_days[impos_index] == i) {
-            DP[i] = DP[i - 1];
-            impos_index++;
-        }
+    for (int day = 0; day <= N; day++) {
+        for (int coupon = 0; coupon < 33; coupon++) {
+            // INT_MAX인 경우는 이렇게 오는 방법이 없음을 의미한다. 따라서 볼 필요 없음
+            if (DP[day][coupon] == INT_MAX) continue;
 
-        // 만약, 이전 날에 쿠폰이 3개가 모였다면 그걸 쓸 수 있다.
-        else if (DP[i - 1].second >= 3) {
-            DP[i] = DP[i - 1];
-            DP[i].second -= 3;
-        }
+            // 1일권을 구매하는 경우
+            if (day + 1 <= N) 
+                DP[day + 1][coupon] = min(DP[day + 1][coupon], DP[day][coupon] + 10000);
+            
+            // 3일권을 구매하는 경우
+            if (day + 3 <= N)
+                DP[day + 3][coupon + 1] = min(DP[day + 3][coupon + 1], DP[day][coupon] + 25000);
 
-        // 다 아니라면, 1일, 3일, 5일 중 어떤 것을 사용할지 정한다.
-        else {
-            int min_money = -1;
-            int min_ticket = -1;
-            for (auto ticket : ticket_list) {
-                int money_expected = DP[i - ticket[1]].first + ticket[0];
-                int ticket_expected = DP[i - ticket[1]].second + ticket[2];
+            // 5일권을 구매하는 경우
+            if (day + 5 <= N)
+                DP[day + 5][coupon + 2] = min(DP[day + 5][coupon + 2], DP[day][coupon] + 37000);
 
-                // 돈이 제일 적은 것이 우선이다.
-                if (min_money > money_expected || min_money == -1) {
-                    min_money = money_expected;
-                    min_ticket = ticket_expected;
-                }
-
-                // 돈이 같다면, 티켓 갯수로 생각한다.
-                else if (money_expected == min_money && min_ticket < ticket_expected) {
-                    min_ticket = ticket_expected;
-                }
-            }
-            // 제일 작은 걸 골랐다면, 저장한다
-            DP[i].first = min_money;
-            DP[i].second = min_ticket;
+            // 내일이 쉬는 날이라면 이렇게도 적용 가능하다
+            if (day + 1 <= N && impos_set.count(day + 1)) 
+                DP[day + 1][coupon] = min(DP[day + 1][coupon], DP[day][coupon]);
+            
+            // 쿠폰이 사용 가능하면, 사용을 고려한다.
+            if (day + 1 <= N && coupon >= 3) 
+                DP[day + 1][coupon - 3] = min(DP[day + 1][coupon - 3], DP[day][coupon]);
         }
     }
     
-    // 결과 체크하기
-    for (int i = 0; i <= N; i++) {
-        cout << "DP[" << i << "].money = " << DP[i].first << endl;
-        cout << "DP[" << i << "].ticket = " << DP[i].second << endl;
-        cout << endl;
+    // // 결과 체크하기
+    // for (int i = 0; i <= N; i++) {
+    //     for (int j = 0; j < 33; j++) {
+    //         if (DP[i][j] == INT_MAX) continue;
+    //         cout << "DP[" << i << "][" << j << "] = " << DP[i][j] << endl;
+    //     }
+    //     cout << endl;
+    // }
+
+    // 결과 출력하기
+    int min_money = INT_MAX;
+    for (int coupon = 0; coupon < 33; coupon++) {
+        if (DP[N][coupon] < min_money) {
+            min_money = DP[N][coupon];
+        }
     }
+
+    cout << min_money << endl;
     return 0;
 }
