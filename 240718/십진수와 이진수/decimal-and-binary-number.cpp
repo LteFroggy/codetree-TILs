6971f1230c424e57f196a474951a290ae1c2ef47
hojong <33508056+LteFroggy@users.noreply.github.com>
2024-07-18 17:04:44 +0900

    Title: 십진수와 이진수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/십진수와 이진수/decimal-and-binary-number.cpp
+++ 240718/십진수와 이진수/decimal-and-binary-number.cpp
@@ -14,22 +14,18 @@ int main() {
     string sInput;
     cin >> sInput;
     
-    // 먼저 16배 해주기
+    // 먼저 16배 해주고, 덧셈 시 자리수가 넘어갈 경우를 대비해 여분 공간 마련하기
     string sResult = sInput + "0000";
+    sResult = '0' + sResult;
     // 더할 때, 자리수가 맞도록 추가해주기
-    sInput = "0000" + sInput;
+    sInput = "00000" + sInput;
     
     // 거기에 원본 값 더하기
-    for (int i = sResult.size() - 1; i >= 0; i--) {
+    for (int i = sResult.size() - 1; i > 0; i--) {
         sResult[i] = char(int(sResult[i] - '0') + int(sInput[i] -'0') + '0');
         // cout << sResult.size() - i << " 번째 자리 값은 " << char(int(sResult[i] - '0') + int(sInput[i] -'0') + '0') << endl;
-    }
 
-    // 근데, 그냥 값을 더하기만 했으므로 2 이상의 값이 나올 수 있다. 따라서 올림을 좀 해 주어야 함
-    // 자리수가 넘어갈 경우를 대비해 여분 공간 마련하기
-    sResult = '0' + sResult;
-
-    for (int i = sResult.size() - 1; i > 0; i--) {
+        // 근데, 그냥 값을 더하기만 했으므로 2 이상의 값이 나올 수 있다. 따라서 올림을 좀 해 주어야 함
         if (sResult[i] - '0' > 1) {
             int div = int(sResult[i] - '0') / 2;
             int remain = int(sResult[i] - '0') % 2;
