18e3e5b41e78efb6f9694a42c9a9d1d7ae04544b
hojong <33508056+LteFroggy@users.noreply.github.com>
2024-07-30 17:28:41 +0900

    Title: 줄 겹치게 접기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240730/줄 겹치게 접기/rope-folding.cpp
+++ 240730/줄 겹치게 접기/rope-folding.cpp
@@ -47,40 +47,89 @@ int main() {
         // cout << "현재 점의 위치는 " << point_now << endl;
         // cout << "다음 점의 위치는 " << point_next << endl;
 
-        // 이제 이것보다 왼쪽에 있는 점들과의 중간점을 모두 찾아 접어볼 것이다.
-        for (int j = i + 1; j < N; j++) {
-            double middle_point = double(point_now + points[j]) / 2;
-            // 대신, 중간점의 좌표가 다음 점의 위치 초과가 되면 안됨. 그럼 다음 점의 영역이 되니까 다음 점에서 체크할 것이기 때문
-            if (middle_point > point_next) break;
+        // 이제 이 점 바로 왼쪽의 있는 점과 중간점을 모두 찾아 접어볼 것이다.   
+        // 다음 점과의 중간점 찾아보기
+        int j = i + 1;
+        double middle_point = double(point_now + points[j]) / 2;
 
-            // 그렇지 않다면 모든 점이 겹치는지 확인해본다.
-            int diff = 0;
-            int left_start(j), right_start(i);
-            // cout << "ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ" << endl;
-            // cout << j + 1 << "번과의 중간점은 " << middle_point << "로, " << point_next << "이하를 만족한다." << endl;
-            
-            // 점 하나하나가 일치하는지 확인
-            bool flag = true;
-            while (true) {
-                diff++;
-                // 보려는 점의 범위를 다 봤으면 while문을 끝낸다
-                if (left_start + diff >= N || right_start - diff < 0) break;
+        // 모든 점이 겹치는지 확인하기
+        int diff = 0;
+        int left_start(j), right_start(i);
+        // cout << "ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ" << endl;
+        // cout << i + 1 << "번째 점과 " << j + 1 << "번의 중간점은 " << middle_point << "로, " << point_next << "이하를 만족한다." << endl;
+        // cout << i + 1 << " : " << point_now << ", 중간점과의 거리는 " << middle_point - point_now << endl;
+        // cout << j + 1 << " : " << points[j] << ", 중간점과의 거리는 " << points[j] - middle_point << endl;
+        
+        // 점 하나하나가 일치하는지 확인
+        bool flag = true;
+        while (true) {
+            diff++;
+            // 줄의 길이가 넘어가면 더 볼 필요 없음.
+            if (left_start + diff >= N || right_start - diff < 0) break;
 
-                // 그렇지 않으면 추가로 확인한다.
-                double left_loc = points[left_start + diff] - middle_point;
-                double right_loc = middle_point - points[right_start - diff];
+            // 그렇지 않으면 추가로 확인한다.
+            double left_loc = points[left_start + diff] - middle_point;
+            double right_loc = middle_point - points[right_start - diff];
+            // cout << "----" << endl;
+            // cout << right_start - diff + 1 << " : " << points[right_start - diff] << ", 중간점과의 거리는 " << right_loc << endl;
+            // cout << left_start + diff + 1 << " : " << points[left_start + diff] << ", 중간점과의 거리는 " << left_loc << endl;
 
-                // 만약 두 점이 겹치지 못한다면, flag를 false로 바꾸고 반복문을 종료한다.
-                if (left_loc != right_loc) {
-                    flag = false;
-                    break;
-                }
+            // 만약 두 점이 겹치지 못한다면, flag를 false로 바꾸고 반복문을 종료한다.
+            if (left_loc != right_loc) {
+                // cout << "다르므로 종료" << endl << endl;
+                flag = false;
+                break;
             }
+            // cout << "같으므로 다음 점 체크" << endl;
+        }
 
-            // while문이 끝났을 때 flag가 true이면, 모든 점이 겹쳐지는 것이므로 answer를 1 증가시킨다.
-            if (flag) {
-                answer++;
+        // while문이 끝났을 때 flag가 true이면, 모든 점이 겹쳐지는 것이므로 answer를 1 증가시킨다.
+        if (flag) {
+            // cout << "모두 같으므로 answer를 1 증가시킨다" << endl << endl;
+            answer++;
+        }
+
+        // 다음 점을 기준으로 접었을 때를 기준으로 생각해보기
+        // 근데 다음 점이 마지막 점이면, 접으면 안 됨
+        if (i + 1 == N - 1) continue;
+        // 다음 점과의 중간점 찾아보기
+        j = i + 2;
+        middle_point = points[i + 1];
+
+        // 모든 점이 겹치는지 확인하기
+        diff = -1;
+        left_start = j;
+        right_start = i;
+        // cout << "ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ" << endl;
+        // cout << j << "번 점을 기준으로 접었을 때이다. 중간점은 " << middle_point << endl;
+        
+        // 점 하나하나가 일치하는지 확인
+        flag = true;
+        while (true) {
+            diff++;
+            // 줄의 길이가 넘어가면 더 볼 필요 없음.
+            if (left_start + diff >= N || right_start - diff < 0) break;
+
+            // 그렇지 않으면 추가로 확인한다.
+            double left_loc = points[left_start + diff] - middle_point;
+            double right_loc = middle_point - points[right_start - diff];
+            // cout << "----" << endl;
+            // cout << right_start - diff + 1 << " : " << points[right_start - diff] << ", 중간점과의 거리는 " << right_loc << endl;
+            // cout << left_start + diff + 1 << " : " << points[left_start + diff] << ", 중간점과의 거리는 " << left_loc << endl;
+
+            // 만약 두 점이 겹치지 못한다면, flag를 false로 바꾸고 반복문을 종료한다.
+            if (left_loc != right_loc) {
+                // cout << "다르므로 종료" << endl << endl;
+                flag = false;
+                break;
             }
+            // cout << "같으므로 다음 점 체크" << endl;
+        }
+
+        // while문이 끝났을 때 flag가 true이면, 모든 점이 겹쳐지는 것이므로 answer를 1 증가시킨다.
+        if (flag) {
+            // cout << "모두 같으므로 answer를 1 증가시킨다" << endl << endl;
+            answer++;
         }
     }
 
@@ -89,3 +138,54 @@ int main() {
     cout << answer << endl;    
     return 0;
 }
+
+
+
+
+
+/* for문 원본
+ // 이제 이 점 바로 왼쪽의 있는 점과 중간점을 모두 찾아 접어볼 것이다.
+        for (int j = i + 1; j < i + 2; j++) {
+            double middle_point = double(point_now + points[j]) / 2;
+            // 대신, 중간점의 좌표가 다음 점의 위치 초과가 되면 안됨. 그럼 다음 점의 영역이 되니까 다음 점에서 체크할 것이기 때문
+            if (middle_point > point_next) break;
+
+            // 그렇지 않다면 모든 점이 겹치는지 확인해본다.
+            int diff = 0;
+            int left_start(j), right_start(i);
+            cout << "ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ" << endl;
+            cout << i + 1 << "번째 점과 " << j + 1 << "번의 중간점은 " << middle_point << "로, " << point_next << "이하를 만족한다." << endl;
+            cout << i + 1 << " : " << point_now << ", 중간점과의 거리는 " << middle_point - point_now << endl;
+            cout << j + 1 << " : " << points[j] << ", 중간점과의 거리는 " << points[j] - middle_point << endl;
+            
+            // 점 하나하나가 일치하는지 확인
+            bool flag = true;
+            while (true) {
+                diff++;
+                // 줄의 길이가 넘어가면 더 볼 필요 없음.
+                if (left_start + diff >= N || right_start - diff < 0) break;
+
+                // 그렇지 않으면 추가로 확인한다.
+                double left_loc = points[left_start + diff] - middle_point;
+                double right_loc = middle_point - points[right_start - diff];
+                cout << "----" << endl;
+                cout << right_start - diff + 1 << " : " << points[right_start - diff] << ", 중간점과의 거리는 " << right_loc << endl;
+                cout << left_start + diff + 1 << " : " << points[left_start + diff] << ", 중간점과의 거리는 " << left_loc << endl;
+
+                // 만약 두 점이 겹치지 못한다면, flag를 false로 바꾸고 반복문을 종료한다.
+                if (left_loc != right_loc) {
+                    cout << "다르므로 종료" << endl << endl;
+                    flag = false;
+                    break;
+                }
+                cout << "같으므로 다음 점 체크" << endl;
+            }
+
+            // while문이 끝났을 때 flag가 true이면, 모든 점이 겹쳐지는 것이므로 answer를 1 증가시킨다.
+            if (flag) {
+                cout << "모두 같으므로 answer를 1 증가시킨다" << endl << endl;
+                answer++;
+            }
+        }
+
+*/
